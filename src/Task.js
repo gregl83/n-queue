6759d7ee6478a4efefb50b67a09a33d3e4a03b90
Gregory Langlais <general@gregorylanglais.com>
2015-04-18 19:49:17 -0400

    task object (enforce structure)

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+function Task() {
+  // todo constructor
+}
+
+module.exports = Task;
---
+++ test/task.js
@@ -0,0 +1,12 @@
+var should = require('should');
+var sinon = require('sinon');
+var config = require('config');
+
+var Task = require('../src/Task');
+
+describe('task', function() {
+  it('new task', function(done) {
+    // todo
+    done();
+  });
+});
