1acbb83e8833a84e109b2f77f83d59d3f3b755fc
Gregory Langlais <general@gregorylanglais.com>
2015-04-18 20:51:02 -0400

    task structure skeleton

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,63 @@
-function Task() {
-  // todo constructor
+/*
+{
+ meta: {
+   attempts: {
+    max: 3
+   },
+   sets: [
+     {
+      set: scheduled,
+      date: Date
+     },
+     {
+      set: queued,
+      date: Date
+     },
+     {
+      set: processing,
+      date: Date,
+      attempts: [
+        // dates ...
+      ]
+     },
+     {
+      set: done,
+      date: Date
+     }
+   ]
+ },
+ data: {
+  // ...
+ }
 }
+ */
+
+
+/**
+ * Queue Task object
+ *
+ * @param {string} task in JSON string format
+ * @constructor
+ */
+function Task(task) {
+  task = (task) ? JSON.encode(task) : {};
+
+  this.meta = ('object' === typeof task.meta && !Array.isArray(task.meta)) ? task.meta : {};
+  this.data = ('object' === typeof task.data && !Array.isArray(task.data)) ? task.data : {};
+}
+
+
+/**
+ * Convert Task to JSON string
+ *
+ * @returns {string} task in JSON string format
+ */
+Task.prototype.toString = Task.prototype.toJSON = function() {
+  return JSON.toString({
+    meta: this.meta,
+    data: this.data
+  });
+};
+
 
 module.exports = Task;
