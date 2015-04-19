5bf8ac4391ccf120e693f8203fcfe7e8f2dc7601
Gregory Langlais <general@gregorylanglais.com>
2015-04-18 21:53:17 -0400

    new task no args/params test

--- test/task.js
+++ test/task.js
@@ -6,45 +6,54 @@ var Task = require('../src/Task');
 
 
 describe('task', function() {
+  it('new task', function(done) {
+    var task = new Task();
+
+    (task.meta).should.be.eql({});
+    (task.data).should.be.eql({});
+
+    done();
+  });
+
   it('new task object task param', function(done) {
-    var task = {meta: {/* meta in array */}, data: {/* data in array */}};
+    var oldTask = {meta: {/* meta in array */}, data: {/* data in array */}};
 
-    task = new Task(task);
+    var task = new Task(oldTask);
 
-    (task.meta).should.be.eql(task.meta);
-    (task.data).should.be.eql(task.data);
+    (task.meta).should.be.eql(oldTask.meta);
+    (task.data).should.be.eql(oldTask.data);
 
     done();
   });
 
   it('new task string task param', function(done) {
-    var task = {meta: {/* meta in array */}, data: {/* data in array */}};
-    var taskString = JSON.stringify(task);
+    var oldTask = {meta: {/* meta in array */}, data: {/* data in array */}};
+    var taskString = JSON.stringify(oldTask);
 
-    task = new Task(taskString);
+    var task = new Task(taskString);
 
-    (task.meta).should.be.eql(task.meta);
-    (task.data).should.be.eql(task.data);
+    (task.meta).should.be.eql(oldTask.meta);
+    (task.data).should.be.eql(oldTask.data);
 
     done();
   });
 
   it('new task invalid task param', function(done) {
-    var task = [{/* meta in array */}, {/* data in array */}];
+    var oldTask = [{type: "meta" /* meta in array */}, {type: "data" /* data in array */}];
 
-    task = new Task(task);
+    var task = new Task(oldTask);
 
-    (task.meta).should.not.eql(task[0]);
-    (task.data).should.not.eql(task[1]);
+    (task.meta).should.not.eql(oldTask[0]);
+    (task.data).should.not.eql(oldTask[1]);
 
     done();
   });
 
   it('to JSON string', function(done) {
-    var task = {meta: {/* meta in array */}, data: {/* data in array */}};
-    var taskString = JSON.stringify(task);
+    var oldTask = {meta: {/* meta in array */}, data: {/* data in array */}};
+    var taskString = JSON.stringify(oldTask);
 
-    task = new Task(task);
+    var task = new Task(oldTask);
 
     var toString = task.toString();
     var stringify = JSON.stringify(task);
