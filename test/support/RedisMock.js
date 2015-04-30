19adf6182370e1224bcf71d714a0256c8aa4ac75
Gregory Langlais <general@gregorylanglais.com>
2015-04-30 00:26:39 -0400

    redisMock evalsha methods

--- test/support/RedisMock.js
+++ test/support/RedisMock.js
@@ -1,6 +1,9 @@
 function RedisClientMock() {
   var self = this;
 
+  self.lists = {
+    /* key string : list array */
+  };
   self.setsIndex = [
     /* priority number : index number */
   ];
@@ -19,15 +22,71 @@ RedisClientMock.prototype.evalsha = function(args, cb) {
 
   var sha = args.shift();
 
-  if ('string' !== typeof sha) return cb(new Error('ERR syntax error'));
-
-  if ('undefind' === typeof self[sha]) return cb(new Error('ERR syntax error'));
+  if ('string' !== typeof sha || 'undefined' === typeof self[sha]) return cb(new Error('ERR syntax error'));
 
   self[sha](args, function(err, response) {
     if (err) return cb(err);
 
     cb(undefined, response);
   });
+};
+
+
+RedisClientMock.prototype._prpush = function(args, cb) {
+  var self = this;
+
+  var key = args.shift();
+  var priority = args.shift();
+
+  if ('string' !== typeof key || 'string' !== typeof priority) return cb(new Error('ERR syntax error'));
+
+  var keyspace = key + ':' + priority;
+
+  if (!Array.isArray(self.lists[keyspace])) self.lists[keyspace] = [];
+
+  var response = 0;
+
+  args.forEach(function(val) {
+    self.lists[keyspace].push(val);
+    response++;
+  });
+
+  cb(undefined, response);
+};
+
+
+RedisClientMock.prototype._prpoplpush = function(args, cb) {
+  var self = this;
+
+  var keysCount = args.shift();
+  var source = args.shift();
+  var destination = args.shift();
+
+  if ('number' !== typeof keysCount || 'string' !== typeof source || 'string' !== destination) return cb(new Error('ERR syntax error'));
+
+  var response = null;
+
+  args.every(function(val) {
+    var sourceKey = source + ':' + val;
+
+    if ('undefined' === typeof self.lists[sourceKey]) {
+      self.lists[sourceKey] = [];
+      return true;
+    }
+
+    if (0 === self.lists[sourceKey].length) return true;
+
+    var destinationKey = destination + ':' + val;
+    if ('undefined' === typeof self.lists[destinationKey]) self.lists[destinationKey] = [];
+
+    response = self.lists[sourceKey].pop();
+
+    self.lists[destinationKey].unshift(response);
+
+    return false;
+  });
+
+  cb(undefined, response);
 };
 
 
